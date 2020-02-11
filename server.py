f63620b2dd7ff0e55ddd3cd00116924cb4ae9c7d
Chris <chris@speedycomputing.com>
2020-02-11 11:13:43 -0800

    Number Game

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template, request, redirect, session
+import random
+app = Flask(__name__)
+
+app.secret_key = 'codingdojosecret'
+
+@app.route('/')
+def index():
+    if 'num' not in session:
+        session['num'] = random.randint(1,100)
+    return render_template("index.html")
+
+@app.route('/guess', methods=['POST'])
+def guess():
+    session['guess'] = int(request.form['guess'])
+    return redirect('/')
+
+@app.route('/reset')
+def reset():
+    del session['num']
+    del session['guess']
+    return redirect('/')
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+#Post to Mattermost
